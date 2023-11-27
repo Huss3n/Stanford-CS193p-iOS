5d5b147518f7ca2f7b4cce7a89875f16c7dc0c5b
Huss3n <aisackhussein@gmail.com>
2023-11-27 14:36:36 +0300

    Initial commit Cardview isFaceUp

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Muktar Hussein on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
